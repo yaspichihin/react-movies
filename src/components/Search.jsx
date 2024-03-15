c82bcd6305f013217c45cf34ef6743e351896964
Yaroslav <yaspichihin@gmail.com>
2024-03-15 15:16:49 +0300

    add search field and filter by type_content

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,79 @@
-export function Search() {
-    return (
-        <div className="search"></div>
-    )
+import React, { Component } from "react"
+
+class Search extends Component {
+    state = {
+        search: '',
+        type_content: 'all'
+
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleKey = (event) => {
+        if (event.key === 'Enter') {
+            this.props.fn_search(this.state.search, this.state.type_content)
+        }
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="input-field">
+                    <input
+                        className="validte"
+                        placeholder="search"
+                        type="search"
+                        name="search"
+                        value={this.state.search}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKey}
+                    />
+                    <button
+                        className="btn search-btn"
+                        onClick={() => this.props.fn_search(this.state.search, this.state.type_content)}
+                    >
+                    Search
+                    </button>
+                </div>
+                <div>
+                    <label>
+                        <input 
+                            className="with-gap"
+                            name="type_content"
+                            type="radio"
+                            value="all"
+                            onChange={this.handleChange}
+                            checked={this.state.type_content === 'all'}
+                        />
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input 
+                            className="with-gap"
+                            name="type_content"
+                            type="radio"
+                            value="movie"
+                            onChange={this.handleChange}
+                            checked={this.state.type_content === 'movie'}
+                        />
+                        <span>Movie</span>
+                    </label>
+                    <label>
+                        <input 
+                            className="with-gap"
+                            name="type_content"
+                            type="radio"
+                            value="series"
+                            onChange={this.handleChange}
+                            checked={this.state.type_content === 'series'}
+                        />
+                        <span>Series</span>
+                    </label>
+                </div>
+            </div>
+        )
+    }
 }
+
+export { Search }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,26 +1,36 @@
 import React, { Component } from "react"
 import { Movies } from '../components/Movies'
 import { Preloader } from '../components/Preloader'
+import { Search } from '../components/Search'
 
 
 class Main extends Component {
+    token = process.env.REACT_APP_OMDB_TOKEN
+
     state = {
         movies: []
     }
 
-    componentDidMount() {
-        const token = process.env.REACT_APP_OMDB_TOKEN
-
-        fetch(`http://www.omdbapi.com/?apikey=${token}&s=harry`)
+    fn_search_movies = (value, type = 'all') => {
+        fetch(`http://www.omdbapi.com/?apikey=${this.token}&s=${value}${type === 'all' ? '' : `&type=${type}`}`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search}))
-        
     }
+
+    // componentDidMount() {
+    //     const token = process.env.REACT_APP_OMDB_TOKEN
+
+    //     fetch(`http://www.omdbapi.com/?apikey=${token}&s=harry`)
+    //         .then(response => response.json())
+    //         .then(data => this.setState({movies: data.Search}))
+        
+    // }
     
     render() {
         const { movies } = this.state;
         return (
             <main className="container content" >
+                <Search fn_search={ this.fn_search_movies } />
                 {/* Добавили preloader если список фильмов не прогрузился */}
                 {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />} 
 

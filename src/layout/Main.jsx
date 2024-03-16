83736759ed13c5d0366bed1454182104dbeb7efc
Yaroslav <yaspichihin@gmail.com>
2024-03-17 00:25:22 +0300

    change class Main to func

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,39 +1,40 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Movies } from '../components/Movies'
 import { Preloader } from '../components/Preloader'
 import { Search } from '../components/Search'
 
+const token = process.env.REACT_APP_OMDB_TOKEN
+    
+export function Main() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-class Main extends Component {
-    token = process.env.REACT_APP_OMDB_TOKEN
-
-    state = {
-        movies: [],
-        isLoading: false
-    }
-
-    fn_search_movies = (value, type = 'all') => {
-        this.setState({isLoading: true})
-        fetch(`https://www.omdbapi.com/?apikey=${this.token}&s=${value}${type === 'all' ? '' : `&type=${type}`}`)
+    const fn_search_movies = (value, type = 'all') => {
+        setIsLoading(true);
+        fetch(
+            `https://www.omdbapi.com/` +
+            `?apikey=${token}&` +
+            `s=${value}` +
+            `${type === 'all' ? '' : `&type=${type}`}`
+        )
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search || [], isLoading: false }))
+            .then(data => {
+                setMovies(data.Search || []);
+                setIsLoading(false);
+                }
+            )
             .catch((err) => {
                 console.error(err);
-                this.setState({isLoading: false})
+                setIsLoading(false);
             })
     }
 
-    render() {
-        const { movies, isLoading } = this.state;
-        return (
-            <main className="container content" >
-                <Search fn_search={ this.fn_search_movies } />
-                {/* Добавили preloader если список фильмов не прогрузился */}
-                { isLoading === false ? <Movies movies={movies} /> : <Preloader />} 
+    return (
+        <main className="container content" >
+            <Search fn_search={ fn_search_movies } />
+            {/* Добавили preloader если список фильмов не прогрузился */}
+            { isLoading === false ? <Movies movies={movies} /> : <Preloader />} 
 
-            </main>
-        )
-    }
+        </main>
+    )
 }
-
-export { Main }
